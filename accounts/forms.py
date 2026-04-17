92825c003a41cccf4f4c1a6bb2d0f03421bbe09f
Eno_Marozi <marozieno0@gmail.com>
2026-04-17 09:44:36 +0700

    17042026

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,31 +1,43 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomSigninForm(AuthenticationForm):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.fields["username"].widget.attrs.update({"class":"form-control","max-length":30,"value":"administrator","required":True})
-		self.fields["password"].widget.attrs.update({"class":"form-control","max-length":30,"value":"12345678","required":True})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({"class":"form-control","max-length":30,"value":"administrator","required":True})
+        self.fields["password"].widget.attrs.update({"class":"form-control","max-length":30,"value":"12345678","required":True})
 
 
-class CustomSignupForm(forms.ModelForm):
-	username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={"class":"form-control"}))
-	email = forms.EmailField(label="Email", max_length=100, widget=forms.EmailInput(attrs={"class":"form-control"}))
-	password1 = forms.CharField(label="Password" ,max_length=30, widget=forms.PasswordInput(attrs={"class":"form-control"}))
-	password2 = forms.CharField(label="Konfirm Password", max_length=30, widget=forms.PasswordInput(attrs={"class":"form-control"}))
-
-	class Meta:
-		model = User
-		fields = ["username","email","password1","password2"]
+class CustomSignupForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    no_hp = forms.CharField(
+        label="No HP (+62)",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "+62..."
+        })
+    )
+    tanggal_lahir = forms.DateField(
+        widget=forms.DateInput(attrs={
+            "class": "form-control",
+            "type": "date"
+        })
+    )
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
 
 class CustomProfileForm(forms.ModelForm):
-	firstname = forms.CharField(label="Firstname", max_length=20, widget=forms.TextInput(attrs={"class":"form-control"}))
-	lastname = forms.CharField(label="Lastname", max_length=20, widget=forms.TextInput(attrs={"class":"form-control"}))
-	username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={"class":"form-control"}))
-	email = forms.EmailField(label="Email", max_length=40, widget=forms.EmailInput(attrs={"class":"form-control"}))
-	is_superuser = forms.CharField(label="Is Superuser", max_length=15, widget=forms.TextInput(attrs={"class":"form-control"}))
+    firstname = forms.CharField(label="Firstname", max_length=20, widget=forms.TextInput(attrs={"class":"form-control"}))
+    lastname = forms.CharField(label="Lastname", max_length=20, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={"class":"form-control"}))
+    email = forms.EmailField(label="Email", max_length=40, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    is_superuser = forms.CharField(label="Is Superuser", max_length=15, widget=forms.TextInput(attrs={"class":"form-control"}))
 
-	class Meta:
-		model = User
-		fields = ["firstname","lastname","username","email","is_superuser"]
+    class Meta:
+        model = User
+        fields = ["firstname","lastname","username","email","is_superuser"]
--- accounts/views.py
+++ accounts/views.py
@@ -30,33 +30,22 @@ def signin(request):
 	return render(request, 'account/signin.html', context)
 
 def signup(request):
-	form = CustomSignupForm()
-	context = {
-		"title" : "SignUp Form",
-	}
-	if request.method == "POST":
-		form = CustomSignupForm(data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get("username")
-			email = form.cleaned_data.get("email")
-			password1 = form.cleaned_data.get("password1")
-			password2 = form.cleaned_data.get("password2")
-			today = datetime.today().date()
-			if len(password1) >= 8 and len(password2) >= 8:
-				if password1 == password2:
-					user = User(username=username, email=email, date_joined=today)
-					user.password = make_password(password2)
-					user.save()
-					messages.success(request, "Pendaftaran Berhasil, Silakan Coba Masuk.")
-					return HttpResponseRedirect(reverse('dashboard'))
-				else:
-					messages.error(request, "Pendaftaran Gagal", "Password Tidak Sama dengan Konfirmasi Password.")
-			else:
-				messages.error(request, "Pendaftaran Gagal, Panjang Password minimal 8 Karakter.")
-		else:
-			messages.error(request, "Pendaftaran Gagal, Pastikan lagi Inputannya.")
-
-	return render(request, 'account/signup.html', {'form':form, 'context':context})
+    form = CustomSignupForm()
+    if request.method == "POST":
+        form = CustomSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = form.cleaned_data["email"]
+            user.date_joined = datetime.now()
+            user.save()
+            messages.success(request, "Pendaftaran berhasil, silakan login.")
+            return redirect("signin")
+        else:
+            messages.error(request, "Pendaftaran gagal, cek kembali input Anda.")
+    return render(request, "account/signup.html", {
+        "form": form,
+        "title": "Sign Up"
+    })
 
 def profile(request):
 	if request.user.is_authenticated:
